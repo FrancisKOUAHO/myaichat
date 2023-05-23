53305ebab6ac2847e93ac62be478fb6d11d83e83
Francis KOUAHO <kouahofrancis@gmail.com>
2023-05-23 18:02:47 -0400

    Update dashboard

--- dashboard/src/app/signup/page.tsx
+++ dashboard/src/app/signup/page.tsx
@@ -1,44 +1,9 @@
-import { useState, FormEvent } from "react";
-import { router } from "next/client";
-import showErrorAlert from "@/components/utility/showErrorAlert";
+'use client'
 
-export default function Signup() {
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
-	const [name, setName] = useState("");
+import { useState } from "react";
+
+const Page = () => {
 	const [isLoading, setIsLoading] = useState(false);
-
-	const handleLoginClick = () => {
-		router.push("/");
-	};
-
-	const handleFormSubmit = async (e: FormEvent) => {
-		e.preventDefault();
-		if (!email && !password && !name) {
-			showErrorAlert("Email, Password, and name is required.");
-			return;
-		} else if (!email && !password) {
-			showErrorAlert("Email and Password is required.");
-			return;
-		} else if (!email && !name) {
-			showErrorAlert("Email and name is required.");
-			return;
-		} else if (!email) {
-			showErrorAlert("Email is required.");
-			return;
-		} else if (!name) {
-			showErrorAlert("Name is required.");
-			return;
-		} else if (!password) {
-			showErrorAlert("Password is required.");
-			return;
-		} else if (password.length < 8) {
-			showErrorAlert("Password should be at least 8 characters long.");
-			return;
-		}
-
-		handleLoginClick();
-	};
 
 	return (
 		<div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:p-8 p-8">
@@ -51,12 +16,13 @@ export default function Signup() {
 					</p>
 					<div className="bg-white py-10 px-4 shadow sm:rounded-lg sm:px-10">
 						<div className="mb-6 flex flex-col justify-center items-center border-gray-200 border-offset-2 pt-4">
-							<button id="google-connect" className=" text-black inline-flex items-center bg-white text-sm font-normal border border-gray-300 rounded-sm px-4 py-2">
-								<img className="h-4 w-4 mr-3" src="/googleLogo.png"/>
+							<button id="google-connect"
+											className=" text-black inline-flex items-center bg-white text-sm font-normal border border-gray-300 rounded-sm px-4 py-2">
+								<img className="h-4 w-4 mr-3" src="/googleLogo.png" alt="googleLogo"/>
 								Connexion avec Google
 							</button>
 						</div>
-						<form onSubmit={handleFormSubmit} className="mb-5">
+						<form className="mb-5">
 							{/*<div className="pb-5">
               <label className="pb-[6px] text-black" htmlFor="name">
                 Name*
@@ -78,34 +44,16 @@ export default function Signup() {
 									type="email"
 									className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
 									id="email"
+									name="email"
 									placeholder="Enter your email"
-									onChange={(e) => setEmail(e.target.value)}
-									value={email}
 								/>
 							</div>
-							{/*            <div className="pb-5">
-              <label className="pb-[6px] text-black" htmlFor="password">
-                Password*
-              </label>
-              <input
-                type="password"
-                className="bg-white text-black text-xl border border-slate-300 rounded-md bg-black p-2 pl-5 w-full"
-                id="password"
-                placeholder="Create a password"
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
-              />
-              <p className="text-left pt-[12px] text-[#475467]">
-                Must be at least 8 characters.
-              </p>
-            </div>*/}
-
 							<div>
 								<button
 									type="submit"
 									disabled={isLoading}
 									className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 mt-3"
-									style={{ backgroundColor: "#7F56D9" }}
+									style={{backgroundColor: "#7F56D9"}}
 								>
 									{isLoading ? "Loading..." : "Commencer"}
 								</button>
@@ -114,8 +62,9 @@ export default function Signup() {
 								<span>Vous avez déjà un compte ?&nbsp;</span>
 								<span
 									className="cursor-pointer"
-									onClick={handleLoginClick}
-									style={{ color: "#7F56D9" }}
+									onClick={() => {
+									}}
+									style={{color: "#7F56D9"}}
 								>
                 Se connecter
               </span>
@@ -127,3 +76,5 @@ export default function Signup() {
 		</div>
 	);
 }
+
+export default Page
