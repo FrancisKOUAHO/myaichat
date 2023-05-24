425edaefa5cde6f3ec6f9ef60f07a295794b25cc
Francis KOUAHO <kouahofrancis@gmail.com>
2023-05-24 09:20:28 -0400

    Update dashboard

--- dashboard/app/signup/page.tsx
+++ dashboard/app/signup/page.tsx
@@ -1,21 +1,21 @@
 'use client'
 
-import React from "react";
 
+import { useState } from "react";
 
-export default function Home() {
-	const [isLoading, setIsLoading] = React.useState(false);
+const Page = () => {
+	const [isLoading, setIsLoading] = useState(false);
 
 	return (
 		<div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:p-8 p-8">
-			<div className="grid w-full ">
+			<div className="grid w-full">
 				<div className="sm:mx-auto sm:w-full p-8 sm:max-w-md">
 					<img src='/logo.png' className='mx-auto h-12 w-auto'/>
-					<h1 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Se connecter</h1>
+					<h1 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">{"S'inscrire"}</h1>
 					<p className="mt-5 mb-5 text-center text-sm text-gray-600">
-						Bienvenue à nouveau ! Veuillez saisir vos coordonnées.
+						Commencez votre essai gratuit de 7 jours.
 					</p>
-					<div className='bg-white py-10 px-4 shadow sm:rounded-lg sm:px-10'>
+					<div className="bg-white py-10 px-4 shadow sm:rounded-lg sm:px-10">
 						<div className="mb-6 flex flex-col justify-center items-center border-gray-200 border-offset-2 pt-4">
 							<button id="google-connect"
 											className=" text-black inline-flex items-center bg-white text-sm font-normal border border-gray-300 rounded-sm px-4 py-2"
@@ -27,44 +27,33 @@ export default function Home() {
 						<form className="mb-5">
 							<div className="pb-5">
 								<label className="block text-sm font-medium text-gray-700 text-gray-400 mb-1" htmlFor="email">
-									Email
+									Email*
 								</label>
 								<input
 									type="email"
 									className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
 									id="email"
+									placeholder="Enter your email"
 									name="email"
-									placeholder="Email"
-								/>
-							</div>
-							<div className="pb-5">
-								<label className="block text-sm font-medium text-gray-700 text-gray-400 mb-1" htmlFor="password">
-									Mot de passe
-								</label>
-								<input
-									type="password"
-									className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-									id="password"
-									name="password"
-									placeholder="Password"
 								/>
 							</div>
 							<div>
 								<button
 									type="submit"
+									disabled={isLoading}
 									className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 mt-3"
 									style={{backgroundColor: "#7F56D9"}}
 								>
-									{isLoading ? "Loading..." : "Se connecter"}
+									{isLoading ? "Loading..." : "Commencer"}
 								</button>
 							</div>
-							<div className="text-xs text-gray-400 mt-1 text-center mt-5">
-								<span>{"Vous n'avez pas encore de compte ?"}&nbsp;</span>
+							<div className="text-xs text-gray-400 mt-5 text-center">
+								<span>Vous avez déjà un compte ?&nbsp;</span>
 								<span
 									className="cursor-pointer"
 									style={{color: "#7F56D9"}}
 								>
-                {"S'inscrire"}
+                Se connecter
               </span>
 							</div>
 						</form>
@@ -75,3 +64,5 @@ export default function Home() {
 	);
 
 }
+
+export default Page
