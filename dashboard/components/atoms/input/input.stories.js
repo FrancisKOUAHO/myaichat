bd23b230fc8afc1ac1a328545e6c1e16e67c6399
Francis KOUAHO <kouahofrancis@gmail.com>
2023-05-25 16:48:15 -0400

    updare

--- dashboard/components/atoms/input/input.stories.js
+++
@@ -1,9 +0,0 @@
-import Input from "./input";
-
-export const InputText = () => <Input type={'text'} label={'Input Text'} />
-export const InputPassword = () => <Input type={'password'} label={'Input Password'} />
-
-export default {
-  title: "Atoms/Input",
-  component: Input,
-}
