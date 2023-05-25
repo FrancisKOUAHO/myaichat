1f4953bb90a10a0d3bcbae05e2f78684e665f917
Francis KOUAHO <kouahofrancis@gmail.com>
2023-05-24 21:01:20 -0400

    updare

--- backend/config/l5-swagger.php
+++ backend/config/l5-swagger.php
@@ -1,7 +1,25 @@
 <?php
 
 return [
-    // ... Vos configurations existantes ...
+    // Autres configurations existantes...
+
+    'defaults' => [
+        'routes' => [
+            // Autres configurations existantes...
+        ],
+
+        'paths' => [
+            // Autres configurations existantes...
+        ],
+
+        'scanOptions' => [
+            // Autres configurations existantes...
+
+            'open_api_spec_version' => env('L5_SWAGGER_OPEN_API_SPEC_VERSION', \L5Swagger\Generator::OPEN_API_DEFAULT_SPEC_VERSION),
+        ],
+
+        // Autres configurations existantes...
+    ],
 
     'documentations' => [
         'default' => [
@@ -10,68 +28,19 @@ return [
             ],
 
             'routes' => [
-                /*
-                 * Route for accessing api documentation interface
-                 */
-                'api' => 'api/documentation',
+                // Configurations de routes
             ],
 
             'paths' => [
-                /*
-                 * Edit to include full URL in ui for assets
-                 */
-                'use_absolute_path' => env('L5_SWAGGER_USE_ABSOLUTE_PATH', true),
-
-                /*
-                 * File name of the generated json documentation file
-                 */
-                'docs_json' => 'api-docs.json',
-
-                /*
-                 * File name of the generated YAML documentation file
-                 */
-                'docs_yaml' => 'api-docs.yaml',
-
-                /*
-                 * Set this to `json` or `yaml` to determine which documentation file to use in UI
-                 */
-                'format_to_use_for_docs' => env('L5_FORMAT_TO_USE_FOR_DOCS', 'json'),
-
-                /*
-                 * Absolute paths to directory containing the swagger annotations are stored.
-                 */
-                'annotations' => [
-                    base_path('app'),
-                ],
+                // Configurations de chemins
             ],
         ],
     ],
 
-    'defaults' => [
-        'routes' => [
-            // ... Autres configurations existantes ...
-        ],
+    // Autres configurations existantes...
 
-        'paths' => [
-            // ... Autres configurations existantes ...
-        ],
+    // Configurations supplémentaires que vous avez fournies
+    'open_api_spec_version' => env('L5_SWAGGER_OPEN_API_SPEC_VERSION', \L5Swagger\Generator::OPEN_API_DEFAULT_SPEC_VERSION),
 
-        'scanOptions' => [
-            // ... Autres configurations existantes ...
-
-            /*
-             * Allows to generate specs either for OpenAPI 3.0.0 or OpenAPI 3.1.0.
-             * By default the spec will be in version 3.0.0
-             */
-            'open_api_spec_version' => env('L5_SWAGGER_OPEN_API_SPEC_VERSION', \L5Swagger\Generator::OPEN_API_DEFAULT_SPEC_VERSION),
-        ],
-
-        /*
-         * Autres configurations existantes ...
-         */
-    ],
-
-    /*
-     * Autres configurations existantes ...
-     */
+    // Autres configurations existantes...
 ];
