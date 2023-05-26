4e9dba0a48acbb367bf6bab74b533e1c8ecb09ad
Voldi Zola <voldi.zola@wister.fr>
2023-05-26 07:11:14 +0200

    tutorial in progress

--- dashboard/app/dashboard/page.tsx
+++ dashboard/app/dashboard/page.tsx
@@ -1,15 +1,21 @@
 "use client"
 
 import React, {useState} from "react";
-import {AiOutlinePlus,AiOutlineFolderAdd,AiOutlineCloseCircle,AiOutlineEye,AiOutlineEdit,AiOutlineDelete,AiOutlineCloudServer} from "react-icons/ai";
+import {AiOutlineRollback,AiOutlineDown,AiOutlinePlus,AiOutlineFolderAdd,AiOutlineCloseCircle,AiOutlineEye,AiOutlineEdit,AiOutlineDelete,AiOutlineCloudServer} from "react-icons/ai";
 import LayoutCustom from "@/layouts/layoutCustom";
 import {Modal} from "../../components/atoms/modal/modal";
 import {Card} from "../../components/atoms/card/card";
+import { Tab, Disclosure } from '@headlessui/react';
 import Image from "next/image";
 import Shopify from "../../public/shopify.png";
+import Theme from "../../public/themes.jpg";
+import Edit from "../../public/edit.jpg";
+import Liquid from "../../public/liquid.jpg";
 
 const Page = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isOpenWidget, setIsOpenWidget] = useState(false);
+  const [isOpenShopify, setIsOpenShopify] = useState(false);
   const [isCardVisible, setCardVisible] = useState(true);
 
   const openModal = () => {
@@ -19,9 +25,27 @@ const Page = () => {
   const closeModal = () => {
     setIsOpen(false);
   };
+
+  //Modal Widget
+  const openModalWidget = () => {
+    setIsOpenWidget(true);
+  };
+
+  const closeModalWidget = () => {
+    setIsOpenWidget(false);
+  };
+
+  //Modal Shopify
+  const openModalShopify = () => {
+    setIsOpenShopify(true);
+  };
+
+  const closeModalShopify = () => {
+    setIsOpenShopify(false);
+  };
   return (
     <LayoutCustom>
-      <div className="w-full overflow-y-auto hidden">
+      <div className="w-full overflow-y-auto ">
         {/*<h2>{"Bilan d'affaires"}</h2>*/}
         <div className="p-40 mx-auto">
           <div className="text-center">
@@ -98,8 +122,8 @@ const Page = () => {
 
       <div className="w-full overflow-y-auto">
         {isCardVisible ? (
-        <Card >
-        <div className="mt-[2%] w-full p-4 text-center bg-white bg-indigo-200  rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+        <Card>
+        <div className="mt-[2%] w-full p-4 text-center bg-indigo-200  rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
           <div className="flex items-center justify-between border-b border-gray-200">
             <span className="font-bold text-gray-900 dark:text-white">My Chatbots</span>
             <a href="#" className="text-white bg-indigo-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
@@ -150,55 +174,191 @@ const Page = () => {
         </div>
       </Card>
         ) : (
-        <div className=" p-4 bg-white rounded-lg md:p-8 dark:bg-gray-800" id="installer">
-          <button id="retour-tab" onClick={() => setCardVisible(true)} className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-[0.675rem] font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+        <div className=" p-4 bg-indigo-200 rounded-lg md:p-8 dark:bg-gray-800" id="installer">
+       {/*   <button id="retour-tab" onClick={() => setCardVisible(true)} className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-[0.675rem] font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
             <AiOutlineCloudServer className="-ml-0.5 mr-1.5 h-5 w-5"/>
             Retour
-          </button>
+          </button>*/}
+          <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <Tab.Group>
+              <div className="bg-gray-50 border-0 border-b border-gray-200 text-gray-900 text-sm rounded-t-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <Tab.List className="text-sm font-medium text-center text-gray-500 divide-x divide-gray-200 rounded-lg sm:flex dark:divide-gray-600 dark:text-gray-400">
+                  <Tab className="inline-block w-full p-4 rounded-tl-lg bg-gray-50 hover:bg-gray-100 focus:outline-none dark:bg-gray-700 dark:hover:bg-gray-600">Instructions pour le widget</Tab>
+                  <Tab className="inline-block w-full p-4 rounded-tl-lg bg-gray-50 hover:bg-gray-100 focus:outline-none dark:bg-gray-700 dark:hover:bg-gray-600">Suivi des instructions de vente <br/> (shopify uniquement)</Tab>
+                  <Tab className="inline-block w-full p-4 rounded-tl-lg bg-gray-50 hover:bg-gray-100 focus:outline-none dark:bg-gray-700 dark:hover:bg-gray-600">
+                    <button id="retour-tab" onClick={() => setCardVisible(true)} className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-[0.675rem] font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                    <AiOutlineRollback className="-ml-0.5 mr-1.5 h-5 w-5"/>
+                    Retour
+                  </button></Tab>
+                </Tab.List>
+                  <Tab.Panels className="border-t border-gray-200 dark:border-gray-600">
+                    <Tab.Panel className="bg-white rounded-lg md:p-8 dark:bg-gray-800">
+                      <button onClick={openModalWidget} className="inline-flex items-center rounded-md mb-2 px-3 py-2 text-[0.675rem] font-semibold text-black shadow-sm hover:bg-indigo-500 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                        <Image src={Shopify} width="15" height="15" alt={''} className="m-1"/>
+                        Instructions Shopify
+                      </button>
+                      <div className="mb-4 mt-4">
+                        <Disclosure>
+                          {({ open }) => (
+                              <>
+                                <Disclosure.Button className="flex w-full justify-between rounded-lg bg-gray-50 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-indigo-100 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+                                  <span>Copier le code suivant</span>
+                                  <AiOutlineDown
+                                      className={`${
+                                          open ? 'rotate-180 transform' : ''
+                                      } h-5 w-5 text-indigo-600`}
+                                  />
+                                </Disclosure.Button>
+                                <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
+                                  <code>
+                                    {'<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>'}
+                                  </code>
 
-          <h2 className="mb-5 text-2xl font-extrabold tracking-tight text-gray-900 dark:text-white">We invest in the
-            world’s potential</h2>
-          <ul role="list" className="space-y-4 text-gray-500 dark:text-gray-400">
-            <li className="flex space-x-2">
-              <svg className="flex-shrink-0 w-4 h-4 text-blue-600 dark:text-blue-500" fill="currentColor"
-                   viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd"
-                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                      clip-rule="evenodd"></path>
-              </svg>
-              <span className="leading-tight">Dynamic reports and dashboards</span>
-            </li>
-            <li className="flex space-x-2">
-              <svg className="flex-shrink-0 w-4 h-4 text-blue-600 dark:text-blue-500" fill="currentColor"
-                   viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd"
-                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                      clip-rule="evenodd"></path>
-              </svg>
-              <span className="leading-tight">Templates for everyone</span>
-            </li>
-            <li className="flex space-x-2">
-              <svg className="flex-shrink-0 w-4 h-4 text-blue-600 dark:text-blue-500" fill="currentColor"
-                   viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd"
-                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                      clip-rule="evenodd"></path>
-              </svg>
-              <span className="leading-tight">Development workflow</span>
-            </li>
-            <li className="flex space-x-2">
-              <svg className="flex-shrink-0 w-4 h-4 text-blue-600 dark:text-blue-500" fill="currentColor"
-                   viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd"
-                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                      clip-rule="evenodd"></path>
-              </svg>
-              <span className="leading-tight">Limitless business automation</span>
-            </li>
-          </ul>
+                                </Disclosure.Panel>
+                              </>
+                          )}
+                        </Disclosure>
+                      </div>
+                      <div className="mb-4 mt-4">
+                        <Disclosure>
+                          {({ open }) => (
+                              <>
+                                <Disclosure.Button className="flex w-full justify-between rounded-lg bg-gray-50 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-indigo-100 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+                                  <span>Ajouter le code dans votre page</span>
+                                  <AiOutlineDown
+                                      className={`${
+                                          open ? 'rotate-180 transform' : ''
+                                      } h-5 w-5 text-indigo-600`}
+                                  />
+                                </Disclosure.Button>
+                                <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
+                                  <pre>
+                                    Collez le code MyAiChat ci-dessus dans votre page.
+                                  </pre>
+                                  <pre className="mb-4">
+                                    {"Vous pouvez l'ajouter dans la section de l'en-tête / head  HTML."}
+                                  </pre>
+                                  <code>
+                                    {"If the chatbot is not showing, try to reset your cache. You can also contact our support team."}
+                                  </code>
+                                </Disclosure.Panel>
+                              </>
+                          )}
+                        </Disclosure>
+                      </div>
+                      <div className="mb-4 mt-4">
+                        <div
+                            className="flex p-4 mb-4 text-sm text-yellow-800 border border-yellow-300 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300 dark:border-yellow-800"
+                            role="alert">
+                          <svg aria-hidden="true" className="flex-shrink-0 inline w-5 h-5 mr-3" fill="currentColor"
+                               viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                  d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+                                  clip-rule="evenodd"></path>
+                          </svg>
+                          <span className="sr-only">Info</span>
+                          <div>
+                            <span className="font-medium">Attention !</span>
+                            {"Si vous avez besoin d'aide, veuillez nous contacter à l'adresse suivante"} <b className="font-medium">contact@myaichat.io</b>
+                          </div>
+                        </div>
+                      </div>
+                    </Tab.Panel>
+                    <Tab.Panel className="bg-white rounded-lg md:p-8 dark:bg-gray-800">
+                      <button onClick={openModalShopify}  className="inline-flex items-center rounded-md mb-2 px-3 py-2 text-[0.675rem] font-semibold text-black shadow-sm hover:bg-indigo-500 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                        <Image src={Shopify} width="15" height="15" alt={''} className="m-1"/>
+                        Instructions Shopify
+                      </button>
+                      <div className="mb-4 mt-4">
+                        <Disclosure>
+                          {({ open }) => (
+                              <>
+                                <Disclosure.Button className="flex w-full justify-between rounded-lg bg-gray-50 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-indigo-100 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+                                  <span>Copier le code suivant</span>
+                                  <AiOutlineDown
+                                      className={`${
+                                          open ? 'rotate-180 transform' : ''
+                                      } h-5 w-5 text-indigo-600`}
+                                  />
+                                </Disclosure.Button>
+                                <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
+                                  <code>
+                                    {' If kljlkjskskdskjdk'+ '<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>'}
+                                  </code>
+
+                                </Disclosure.Panel>
+                              </>
+                          )}
+                        </Disclosure>
+                      </div>
+                      <div className="mb-4 mt-4">
+                        <Disclosure>
+                          {({ open }) => (
+                              <>
+                                <Disclosure.Button className="flex w-full justify-between rounded-lg bg-gray-50 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-indigo-100 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+                                  <span>{"Allez dans les paramètres > Checkout Statut de la page de commande"}</span>
+                                  <AiOutlineDown
+                                      className={`${
+                                          open ? 'rotate-180 transform' : ''
+                                      } h-5 w-5 text-indigo-600`}
+                                  />
+                                </Disclosure.Button>
+                                <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
+                                  <pre>
+                                 {'Collez le code MyAiChat dans "Script supplémentaire".'}
+                                  </pre>
+                                </Disclosure.Panel>
+                              </>
+                          )}
+                        </Disclosure>
+                      </div>
+                      <div className="mb-4 mt-4">
+                        <div
+                            className="flex p-4 mb-4 text-sm text-yellow-800 border border-yellow-300 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300 dark:border-yellow-800"
+                            role="alert">
+                          <svg aria-hidden="true" className="flex-shrink-0 inline w-5 h-5 mr-3" fill="currentColor"
+                               viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                  d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+                                  clip-rule="evenodd"></path>
+                          </svg>
+                          <span className="sr-only">Info</span>
+                          <div>
+                            <span className="font-medium">Attention !</span>
+                            {"Si vous avez besoin d'aide, veuillez nous contacter à l'adresse suivante"} <b className="font-medium">contact@myaichat.io</b>
+                          </div>
+                        </div>
+                      </div>
+                    </Tab.Panel>
+                    <Tab.Panel className="bg-white rounded-lg md:p-8 dark:bg-gray-800">Content 3</Tab.Panel>
+                  </Tab.Panels>
+              </div>
+            </Tab.Group>
+          </div>
         </div>
         )}
       </div>
+
+      <Modal isOpen={isOpenWidget} closeModal={closeModalWidget}>
+        <div className=" max-w-xl px-20 py-8 m-20 overflow-hidden bg-white rounded-lg shadow-xl 2xl:max-w-2xl">
+          <div onClick={closeModalWidget} className="flex justify-start space-x-4">
+            <button className="text-gray-600 focus:outline-none hover:text-gray-700">
+              <AiOutlineCloseCircle/>
+            </button>
+          </div>
+        </div>
+      </Modal>
+
+      <Modal isOpen={isOpenShopify} closeModal={closeModalShopify}>
+        <div className=" max-w-xl px-20 py-8 m-20 overflow-hidden bg-white rounded-lg shadow-xl 2xl:max-w-2xl">
+          <div onClick={closeModalShopify} className="flex justify-start space-x-4">
+            <button className="text-gray-600 focus:outline-none hover:text-gray-700">
+              <AiOutlineCloseCircle/>
+            </button>
+          </div>
+          Voldi
+        </div>
+      </Modal>
     </LayoutCustom>
   )
 }
