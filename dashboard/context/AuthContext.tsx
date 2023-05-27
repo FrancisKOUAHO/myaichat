f8f5a6f38e315b4f601c9508a0822b21cb8a4d6f
Francis KOUAHO <kouahofrancis@gmail.com>
2023-05-27 16:08:04 -0400

    ok

--- dashboard/context/AuthContext.tsx
+++ dashboard/context/AuthContext.tsx
@@ -1,39 +1,27 @@
 import { createContext, ReactNode, useContext, useState } from 'react';
-import axios from 'axios';
+import { api } from "@/config/api";
+import { useMutation } from "@tanstack/react-query";
 
 export const AuthContext = createContext<any>({});
 
 export const useAuth = () => useContext(AuthContext);
 
 export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
-  const [token, setToken] = useState<string | null>(null);
 
-  const login = async (token: string) => {
-    try {
-      const response = await axios.get(`/api/login-with-token/${token}`);
-
-      if (response.status === 200) {
-        localStorage.setItem('token', response.data.user.login_token);
-        setToken(response.data.user.login_token);
-      }
-    } catch (error: any) {
-      console.log('error', error);
+  const loginMutation = useMutation((email) =>
+      api.post('auth/magic-link', {email}),
+    {
+      onSuccess: (data) => {
+        console.log('data', data)
+      },
+      onError: (error) => {
+        console.log('error', error);
+      },
     }
-  };
-
-  const requestLoginLink = async (email: string) => {
-    try {
-      const response = await axios.post('/api/request-login-link', { email });
-      if (response.status === 200) {
-        console.log(response.data.message);
-      }
-    } catch (error: any) {
-      console.log('error', error);
-    }
-  };
+  );
 
   return (
-    <AuthContext.Provider value={{ login, requestLoginLink }}>
+    <AuthContext.Provider value={{ loginMutation }}>
       {children}
     </AuthContext.Provider>
   );
