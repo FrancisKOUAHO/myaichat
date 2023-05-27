b7f6436c28b317dfbaca626d3741c60e5d58f471
Francis KOUAHO <kouahofrancis@gmail.com>
2023-05-27 14:47:21 -0400

    updare

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -27,7 +27,7 @@ class CreateUsersTable extends Migration
      *
      * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('users');
     }
