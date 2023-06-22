51397126ea21cb988cc5b01b91242e3b5039c730
Francis KOUAHO <kouahofrancis@gmail.com>
2023-06-22 14:07:41 -0400

    protected key api

--- backend/app/Http/Controllers/APIController.php
+++ backend/app/Http/Controllers/APIController.php
@@ -2,11 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class APIController extends Controller
 {
-    public function envoyerCleAPI(Request $request)
+    public function envoyerCleAPI()
     {
         $cleAPI = 'qMQPsCk4m1rp24QXQfseT3BlbkFJm65u0wjrVoF44BHcIo1d';
         return response()->json(['cleAPI' => $cleAPI]);
