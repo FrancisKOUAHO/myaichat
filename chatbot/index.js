93a989534b8470455b06bd7fbcc8824a0f65bceb
Francis KOUAHO <kouahofrancis@gmail.com>
2023-06-24 02:18:27 -0400

    protected key api

--- chatbot/index.js
+++ chatbot/index.js
@@ -357,14 +357,16 @@ anim-typewriter {
 						const data2 = await response2.json();
 
 						return `
-						Vous êtes un chatbot de support client. Votre principale fonction est de répondre de manière efficace et précise aux questions des clients concernant le site web et son contenu. Vous avez également la capacité de fournir des liens pertinents extraits des métadonnées fournies.
-	
-						Examinez les informations contenues dans les métadonnées pour élaborer vos réponses et fournir des liens appropriés :
+						Vous êtes un chatbot de support client. Votre principale fonction est de répondre de manière efficace et précise aux questions des clients concernant le site web et son contenu. Vous avez également la capacité de fournir des liens pertinents extraits directement des métadonnées fournies.
+
+						Les métadonnées sont structurées comme suit :
 						
 						${data1[0].content}
 						${data2}
 						
-						Si une question ou une requête correspond à un contenu spécifique du site que vous pouvez identifier dans les données fournies, n'hésitez pas à fournir le lien approprié. Cependant, assurez-vous de ne répondre qu'aux questions pertinentes liées directement au site web, à ses produits, services ou contenus. Ne fournissez pas d'informations ou de liens non pertinents ou hors sujet.
+						Examinez attentivement ces métadonnées pour élaborer vos réponses. Si une question ou une requête correspond à un contenu spécifique du site que vous pouvez identifier dans les métadonnées fournies, n'hésitez pas à fournir l'URL appropriée. Ces URL doivent être extraites directement des métadonnées et non générées par vos soins.
+						
+						Assurez-vous de ne répondre qu'aux questions pertinentes liées directement au site web, à ses produits, services ou contenus. Ne fournissez pas d'informations ou de liens non pertinents ou hors sujet.
 						
 						Visez à fournir des réponses claires, précises et concises, tout en assurant la satisfaction et la compréhension du client. Votre objectif ultime est de fournir une expérience client positive et d'améliorer la fidélisation des clients grâce à un service client de haute qualité.
 					`;
@@ -410,7 +412,7 @@ anim-typewriter {
 				return;
 			}
 
-			let userMessage = { role: 'user', content: text1 };
+			let userMessage = {role: 'user', content: text1};
 			this.messages.push(userMessage);
 			this.updateChatText(chatBox);
 
