1267237afa0eb821b3bed3fafe9697085fdeb977
Francis KOUAHO <kouahofrancis@gmail.com>
2023-06-28 15:00:39 -0400

    chatV2

--- chatbot-loader/chatbot-loader.js
+++ chatbot-loader/chatbot-loader.js
@@ -1,7 +1,9 @@
 const iframe = document.createElement('iframe');
 iframe.src = 'https://chatbot-tawny-seven.vercel.app/';
-iframe.style.width = '387px';
-iframe.style.height = '387px';
+iframe.style.width = '352px';
+iframe.style.maxWidth = '400px';
+iframe.style.height = '506px';
+iframe.style.maxHeight = '506px';
 iframe.style.position = 'fixed';
 iframe.style.bottom = '0';
 iframe.style.right = '0';
