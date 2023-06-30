d837e476f137488e984285339d060f772bca5b9c
Francis KOUAHO <kouahofrancis@gmail.com>
2023-06-30 11:05:08 -0400

    chatV2

--- chatbot/src/app/page.tsx
+++ chatbot/src/app/page.tsx
@@ -1,25 +1,7 @@
 "use client";
 
-import { useEffect } from "react";
-
-const getUrls = () =>{
-  const url = window.location.href;
-  const hostname = new URL(url).hostname;
-  const domain = hostname.replace("www.", "").split(".")[0];
-
-  console.log(domain)
-
-  return domain;
-}
-
 const Home = () => {
-
-  useEffect(() => {}, []);
-
   return <div className="bg-white"></div>;
 };
 
-export {
-  Home,
-  getUrls
-}
+export default Home;
