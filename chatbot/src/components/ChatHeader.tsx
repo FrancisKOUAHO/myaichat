d81701418dc6fc335a01a6a46c6512f3d50f5bc1
Francis KOUAHO <kouahofrancis@gmail.com>
2023-07-01 14:30:06 -0400

    get current url + storage cookies info

--- chatbot/src/components/ChatHeader.tsx
+++ chatbot/src/components/ChatHeader.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const ChatHeader = () => {
 
-	const url = window.location.href;
-	const hostname = new URL(url).hostname;
-	const domain = hostname.replace("www.", "").split(".")[0];
+	const siteURL = document.referrer;
+	const hostname = new URL(siteURL).hostname;
+	const domain = hostname.replace('www.', '').split('.')[0];
 
 	return (
 		<div className='w-full bg-black flex gap-3 justify-start items-center text-zinc-800 p-4 border-b border-zinc-300'>
