efd8b89e4e00888094a228bf1cb8a7d26cad0b2c
Francis KOUAHO <kouahofrancis@gmail.com>
2023-08-19 18:17:11 +0200

    sub and account

--- dashboard/utils/AuthGuard.tsx
+++ dashboard/utils/AuthGuard.tsx
@@ -1,14 +1,41 @@
-import { useRouter } from "next/navigation";
+'use client';
+
+import {useRouter} from "next/navigation";
 import { useAuth } from "@/context/AuthContext";
 import { useEffect } from "react";
+import { api } from "@/config/api";
+import { toast } from "react-toastify";
+import { getCookie } from "cookies-next";
+import {AppRouterInstance} from "next/dist/shared/lib/app-router-context";
 
-const AuthGuard: ({children}: { children: any }) => (null | JSX.Element) = ({children}) => {
-	const {isAuthenticated} = useAuth();
-	const router = useRouter();
+const AuthGuard = ({ children }: { children: React.ReactNode }) => { // Mise à jour de la signature
+	const { isAuthenticated } = useAuth();
+	const router: AppRouterInstance = useRouter();
+
+
+	const checkPayment = () => {
+		api.get(`check-payment`, {
+			headers: {
+				'Authorization': `Bearer ${getCookie('access_token')}`
+			}
+		})
+			.then((paymentRes) => {
+				if (paymentRes.data.order.status === "unpaid") {
+					router.push('/dashboard/subscription');
+				}
+			})
+			.catch((paymentError) => {
+				if (paymentError.response && paymentError.response.status === 404) {
+					router.push('/dashboard/subscription');
+				}
+			});
+	}
 
 	useEffect(() => {
 		if (!isAuthenticated()) {
 			router.push('/');
+		} else {
+			checkPayment();
 		}
 	}, [isAuthenticated, router]);
 
