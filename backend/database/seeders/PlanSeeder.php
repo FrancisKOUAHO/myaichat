36f41f44466a393c9773fe5b5ba5da4cd47b9700
Francis KOUAHO <kouahofrancis@gmail.com>
2023-08-21 16:15:52 +0200

    ok pour la prod

--- backend/database/seeders/PlanSeeder.php
+++ backend/database/seeders/PlanSeeder.php
@@ -22,6 +22,80 @@ class PlanSeeder extends Seeder
                 'interval'=>'month',
                 'trial_period_days'=> 15,
                 'lookup_key'=>'Lite',
+                'st_plan_id'=>'price_1NhYlyFdQvV9SdYX19x5YRWy'
+            ],
+            [
+                'name'=>'Starter',
+                'price'=> 39,
+                'interval'=>'month',
+                'trial_period_days'=> 15,
+                'lookup_key'=>'Starter',
+                'st_plan_id'=>'price_1NVXvfFdQvV9SdYXSGsPeiep'
+            ],
+            [
+                'name'=>'Growth',
+                'price'=> 79,
+                'interval'=>'month',
+                'trial_period_days'=> 15,
+                'lookup_key'=>'Growth',
+                'st_plan_id'=>'price_1NVXvPFdQvV9SdYXYO49ndDl'
+            ],
+            [
+                'name'=>'Pro',
+                'price'=> 99,
+                'interval'=>'month',
+                'trial_period_days'=> 15,
+                'lookup_key'=>'Pro',
+                'st_plan_id'=>'price_1NVXvbFdQvV9SdYXXxdHArtW'
+            ],
+            [
+                'name'=>'Lite',
+                'price'=> 132,
+                'interval'=>'year',
+                'trial_period_days'=> 15,
+                'lookup_key'=>'Lite',
+                'st_plan_id'=>'price_1NhYlpFdQvV9SdYXEkqej8ai'
+            ],
+            [
+                'name'=>'Starter',
+                'price'=> 348,
+                'interval'=>'year',
+                'trial_period_days'=> 15,
+                'lookup_key'=>'Starter',
+                'st_plan_id'=>'price_1NVXvIFdQvV9SdYXoDRFqLRC'
+            ],
+            [
+                'name'=>'Growth',
+                'price'=> 708,
+                'interval'=>'year',
+                'trial_period_days'=> 15,
+                'lookup_key'=>'Growth',
+                'st_plan_id'=>'price_1NVXv7FdQvV9SdYXzRKXk2yn'
+            ],
+            [
+                'name'=>'Pro',
+                'price'=> 828,
+                'interval'=>'year',
+                'trial_period_days'=> 15,
+                'lookup_key'=>'Pro',
+                'st_plan_id'=>'price_1NVXvDFdQvV9SdYXufunLNJs'
+            ],
+        ];
+
+        foreach ($plans as $plan) {
+            Plan::create($plan);
+        }
+    }
+
+    /*public function run(): void
+    {
+        $plans = [
+            [
+                'name'=>'Lite',
+                'price'=> 15,
+                'interval'=>'month',
+                'trial_period_days'=> 15,
+                'lookup_key'=>'Lite',
                 'st_plan_id'=>'price_1NgqMlFdQvV9SdYXyE2UuLzz'
             ],
             [
@@ -85,5 +159,5 @@ class PlanSeeder extends Seeder
         foreach ($plans as $plan) {
             Plan::create($plan);
         }
-    }
+    }*/
 }
