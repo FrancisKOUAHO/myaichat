f51985de1c64bdd993ee95ce81f3324d917d231d
FrancisKOUAHO <kouahofrancis@gmail.com>
2023-08-23 18:50:27 +0200

    responsive

--- chatbot_loader/index.js
+++ chatbot_loader/index.js
@@ -1,13 +1,41 @@
-function applyStylesToIframe(iframe) {
-    const screenWidth = window.innerWidth || document.documentElement.clientWidth;
 
-    if (screenWidth >= 768) {
-        iframe.classList.add('desktop');
-        iframe.classList.remove('mobile');
-    } else {
-        iframe.classList.add('mobile');
-        iframe.classList.remove('desktop');
-    }
+
+function applyStylesToIframe(iframe) {
+
+    window.addEventListener('load', () => {
+        let widthWindow = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+        if (widthWindow >= 768) {
+            // Styles pour les écrans de taille desktop
+            iframe.style.border = 'none';
+            iframe.style.opacity = '1';
+            iframe.style.transform = 'scale(1)';
+            iframe.style.height = 'min(704px, calc(100% - 104px))';
+            iframe.style.position = 'fixed';
+            iframe.style.pointerEvents = 'all';
+            iframe.style.transformOrigin = 'right bottom';
+            iframe.style.transition = 'transform 0.3s cubic-bezier(0, 1.2, 1, 1) 0s, opacity 0.2s ease-out 0s';
+            iframe.style.right = '10px';
+            iframe.style.bottom = '0';
+            iframe.style.width = '400px';
+            iframe.style.zIndex = '10000';
+            iframe.style.borderRadius = '0.75rem';
+        } else {
+            // Styles pour les écrans de taille mobile
+            iframe.style.border = 'none';
+            iframe.style.opacity = '1';
+            iframe.style.transform = 'scale(1)';
+            iframe.style.height = '100vh';
+            iframe.style.width = '100%';
+            iframe.style.position = 'fixed';
+            iframe.style.bottom = '0';
+            iframe.style.right = '0';
+            iframe.style.pointerEvents = 'all';
+            iframe.style.transformOrigin = 'right bottom';
+            iframe.style.transition = 'transform 0.3s cubic-bezier(0, 1.2, 1, 1) 0s, opacity 0.2s ease-out 0s';
+            iframe.style.zIndex = '10000';
+            iframe.style.borderRadius = '0.75rem';
+        }
+    });
 }
 
 function createIframe() {
