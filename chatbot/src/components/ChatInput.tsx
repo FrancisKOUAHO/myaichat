9fef1f676ea97fd3eab151196d6faa3a20d265dc
FrancisKOUAHO <kouahofrancis@gmail.com>
2023-08-27 17:25:52 +0200

    add click inputchat

--- chatbot/src/components/ChatInput.tsx
+++ chatbot/src/components/ChatInput.tsx
@@ -14,7 +14,7 @@ interface ChatInputProps extends HTMLAttributes<HTMLDivElement> {
 }
 
 const ChatInput: FC<ChatInputProps> = ({className, ...props}) => {
-    const textareaRef = useRef<HTMLTextAreaElement | null>(null)
+    const inputRef: any = useRef<HTMLTextAreaElement | null>(null)
     const [input, setInput] = useState<string>('')
     const {
         messages,
@@ -79,47 +79,44 @@ const ChatInput: FC<ChatInputProps> = ({className, ...props}) => {
             setInput('')
 
             setTimeout(() => {
-                textareaRef.current?.focus()
+                inputRef.current?.focus()
             }, 10)
         },
         onError: (_, message) => {
             toast.error('Quelque chose s\'est mal passé. Veuillez réessayer.')
             removeMessage(message.id)
-            textareaRef.current?.focus()
+            inputRef.current?.focus()
         },
     })
 
     return (
         <div {...props} className={cn('border-t border-zinc-300 p-2 container-ui-input', className)}>
             <div className='relative mt-4 flex-1 overflow-hidden rounded-lg border-none outline-none'>
-                <TextareaAutosize
-                    ref={textareaRef}
+                <input
+                    ref={inputRef}
                     onClick={() => {
                         if (!input) {
-                            textareaRef.current?.removeAttribute('readonly');
-                            textareaRef.current?.focus();
+                            inputRef.current?.focus();
                         }
                     }}
                     onKeyDown={(e) => {
                         if (e.key === 'Enter' && !e.shiftKey) {
-                            e.preventDefault()
+                            e.preventDefault();
 
                             const message: Message = {
                                 id: nanoid(),
                                 isUserMessage: true,
                                 text: input,
-                            }
+                            };
 
-                            sendMessage(message)
+                            sendMessage(message);
                         }
                     }}
-                    rows={2}
-                    maxRows={4}
                     value={input}
                     disabled={isLoading}
                     onChange={(e) => setInput(e.target.value)}
                     placeholder='Écrire un message...'
-                    className='peer disabled:opacity-50 pr-14 resize-none block w-full border-0 bg-zinc-100 py-1.5 text-gray-900 focus:ring-0 text-sm sm:leading-6'
+                    className='peer disabled:opacity-50 pr-14 block w-full border-0 bg-zinc-100 py-1.5 text-gray-900 focus:ring-0 text-sm sm:leading-6'
                 />
 
                 <div className='absolute inset-y-0 right-0 flex py-1.5 pr-1.5'>
