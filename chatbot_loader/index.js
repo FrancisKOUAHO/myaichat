bdb2cd01e83b2add0697ea5145a26568d40dac84
FrancisKOUAHO <kouahofrancis@gmail.com>
2023-08-28 16:53:55 +0200

    add click inputchat

--- chatbot_loader/index.js
+++ chatbot_loader/index.js
@@ -5,25 +5,46 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const style = document.createElement('style');
     style.innerHTML = `
-    .widget-chat-button {
-      position: fixed;
-      width: 50px;
-      height: 50px;
-      border-radius: 25px;
-      background-color: black;
-      cursor: pointer;
-      z-index: 999999999;
-      transition: all 0.2s ease-in-out 0s;
-      right: 15px;
-      bottom: 20px;
-      transform: scale(1);
-    }
+        .widget-chat-button {
+            position: fixed;
+            width: 50px;
+            height: 50px;
+            border-radius: 25px;
+            background-color: black;
+            cursor: pointer;
+            z-index: 999999999;
+            transition: all 0.2s ease-in-out 0s;
+            right: 15px;
+            bottom: 20px;
+            transform: scale(1);
+        }
 
-    @media (max-width: 767px) {
-      .widget-chat-button {
-        bottom: 10px;
-      }
-    }
+        @media (max-width: 767px) {
+            .widget-chat-button {
+                bottom: 10px;
+            }
+
+            .widget-chat-iframe {
+                width: 100%;
+                right: 0;
+            }
+        }
+
+        .widget-chat-iframe {
+            border: none;
+            opacity: 1;
+            transform: scale(1);
+            height: 100vh;
+            width: 400px;
+            position: fixed;
+            pointer-events: all;
+            transform-origin: right bottom;
+            transition: transform 0.3s cubic-bezier(0, 1.2, 1, 1) 0s, opacity 0.2s ease-out 0s;
+            right: 10px;
+            bottom: 0;
+            z-index: 9999;
+            border-radius: 0.75rem;
+        }
     `;
     document.head.appendChild(style);
 
@@ -43,44 +64,19 @@ document.addEventListener('DOMContentLoaded', function () {
     container.appendChild(button);
 
     const iframe = createIframe();
+    iframe.classList.add('widget-chat-iframe'); // Add the new class to the iframe
     container.appendChild(iframe);
-    applyStylesToIframe(iframe);
 });
 
 function createIframe() {
     const iframe = document.createElement('iframe');
     iframe.src = 'https://ai.myaichat.io'; // or a local URL for development
-    //iframe.src = 'http://localhost:3031'; // or a local URL for development
     iframe.style.display = 'none'; // Start with the iframe hidden
     document.body.appendChild(iframe);
     return iframe;
 }
 
-function applyStylesToIframe(iframe) {
-    function updateStyles() {
-        const widthWindow = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-        const isMobile = widthWindow < 768;
-
-        iframe.style.border = 'none';
-        iframe.style.opacity = '1';
-        iframe.style.transform = 'scale(1)';
-        iframe.style.height = isMobile ? '100vh' : 'min(740px, calc(100% - 104px))';
-        iframe.style.width = isMobile ? '100%' : '400px';
-        iframe.style.position = 'fixed';
-        iframe.style.pointerEvents = 'all';
-        iframe.style.transformOrigin = 'right bottom';
-        iframe.style.transition = 'transform 0.3s cubic-bezier(0, 1.2, 1, 1) 0s, opacity 0.2s ease-out 0s';
-        iframe.style.right = isMobile ? '0' : '10px';
-        iframe.style.bottom = '0';
-        iframe.style.zIndex = '9999';
-        iframe.style.borderRadius = '0.75rem';
-    }
-
-    window.addEventListener('load', updateStyles);
-    window.addEventListener('resize', updateStyles);
-}
-
 function toggleChat() {
-    const iframe = document.querySelector('iframe');
+    const iframe = document.querySelector('.widget-chat-iframe');
     iframe.style.display = iframe.style.display === 'none' ? 'block' : 'none';
 }
