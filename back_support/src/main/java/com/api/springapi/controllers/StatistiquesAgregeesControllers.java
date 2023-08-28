604984ed5eeeb2b95d5d2f4a116341ba209552a1
voldizola <voldizola1@gmail.com>
2023-08-28 20:18:50 +0200

    rajout du chemin orders

--- back_support/src/main/java/com/api/springapi/controllers/StatistiquesAgregeesControllers.java
+++ back_support/src/main/java/com/api/springapi/controllers/StatistiquesAgregeesControllers.java
@@ -6,12 +6,13 @@ import com.api.springapi.dto.SubscriptionDetailDTO;
 import com.api.springapi.services.StatistiquesAgregeesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/stats")
 public class StatistiquesAgregeesControllers {
---
+++ back_support/src/main/java/com/api/springapi/dto/OrderDTO.java
@@ -0,0 +1,13 @@
+package com.api.springapi.dto;
+
+public class OrderDTO {
+    private Long totalOrder;
+
+    public Long getTotalOrders() {
+        return totalOrder;
+    }
+
+    public void setTotalOrders(Long totalOrder) {
+        this.totalOrder = totalOrder;
+    }
+}
