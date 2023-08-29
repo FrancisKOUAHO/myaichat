d36b64518a45f3d6f021c56766e62423b64b1fb8
FrancisKOUAHO <kouahofrancis@gmail.com>
2023-08-29 15:08:33 +0200

    add admin

--- back_support/src/main/java/com/api/springapi/services/EmailService.java
+++ back_support/src/main/java/com/api/springapi/services/EmailService.java
@@ -4,14 +4,16 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import com.api.springapi.models.User;
 
 @Service
 public class EmailService {
 
     private final JavaMailSender mailSender;
-    @Value("${APP_URL}")
-    private String APP_URL;
+
+    @Value("${DASHBOARD_URL}")
+    private String dashboardUrl;
 
     @Autowired
     public EmailService(JavaMailSender mailSender) {
@@ -19,7 +21,7 @@ public class EmailService {
     }
 
     public void sendLoginEmail(User user) {
-        String magicLinkToken = APP_URL + "/verify/?magic_link_token=" + user.getMagicLinkToken();
+        String magicLinkToken = dashboardUrl + "/verify/?magic_link_token=" + user.getMagicLinkToken();
         String messageBody = "Cliquez sur le lien ci-dessous pour vous connecter :\n\n" + magicLinkToken;
 
         SimpleMailMessage message = new SimpleMailMessage();
