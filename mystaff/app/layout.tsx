8c3cf3a595a8a45d9ddaee13dc90d82d687683ce
FrancisKOUAHO <kouahofrancis@gmail.com>
2023-09-25 16:58:39 +0200

    ok mystaff

--- mystaff/app/layout.tsx
+++ mystaff/app/layout.tsx
@@ -1,12 +1,11 @@
 import './globals.css'
 import 'react-toastify/dist/ReactToastify.css'
 
-import React from "react"
+import React from 'react'
 
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ToastContainer } from 'react-toastify'
-
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,17 +13,13 @@ export const metadata: Metadata = {
   title: 'MyaiChat',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="fr">
-      <body className={inter.className}>
-      {children}
-      <ToastContainer />
-      </body>
+    <html lang='fr'>
+    <body className={inter.className}>
+    {children}
+    <ToastContainer />
+    </body>
     </html>
   )
 }
