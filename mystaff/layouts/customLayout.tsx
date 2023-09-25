f3db0fe00776b6d0355a81df6c15e1f3df2d78b0
FrancisKOUAHO <kouahofrancis@gmail.com>
2023-09-25 17:11:08 +0200

    ok authguard

--- mystaff/layouts/customLayout.tsx
+++ mystaff/layouts/customLayout.tsx
@@ -4,32 +4,35 @@ import { UserNav } from '@/components/user-nav'
 import { Tabs } from '@/components/ui/tabs'
 import * as React from 'react'
 import TableNav from '@/components/table-nav'
-import AuthContextProvider from '@/app/context/authContext'
+import AuthContextProvider from '@/context/authContext'
+import AuthGuard from '@/utils/AuthGuard'
 
 export default function customLayout({ children }: { children: any }) {
   return (
     <AuthContextProvider>
-      <div className='flex-col md:flex'>
-        <div className='border-b'>
-          <div className='flex h-16 items-center px-4'>
-            <div className='ml-auto flex items-center space-x-4'>
-              <Search />
-              <UserNav />
+      <AuthGuard>
+        <div className='flex-col md:flex'>
+          <div className='border-b'>
+            <div className='flex h-16 items-center px-4'>
+              <div className='ml-auto flex items-center space-x-4'>
+                <Search />
+                <UserNav />
+              </div>
             </div>
           </div>
-        </div>
-        <div className='flex-1 space-y-4 p-8 pt-6'>
-          <div className='flex items-center justify-between space-y-2'>
-            <h2 className='text-3xl font-bold tracking-tight'>Dashboard</h2>
-            <div className='flex items-center space-x-2'>
+          <div className='flex-1 space-y-4 p-8 pt-6'>
+            <div className='flex items-center justify-between space-y-2'>
+              <h2 className='text-3xl font-bold tracking-tight'>Dashboard</h2>
+              <div className='flex items-center space-x-2'>
+              </div>
             </div>
+            <Tabs defaultValue='apercu' className='space-y-4'>
+              <TableNav />
+              {children}
+            </Tabs>
           </div>
-          <Tabs defaultValue='apercu' className='space-y-4'>
-            <TableNav />
-            {children}
-          </Tabs>
         </div>
-      </div>
+      </AuthGuard>
     </AuthContextProvider>
   )
 
