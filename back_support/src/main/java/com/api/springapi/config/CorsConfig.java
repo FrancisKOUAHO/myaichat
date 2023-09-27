972c85d454335ce13f791ea96b00be5c9c5f54a1
voldizola <voldizola1@gmail.com>
2023-09-27 19:39:47 +0200

    cors fix

--- back_support/src/main/java/com/api/springapi/config/CorsConfig.java
+++ back_support/src/main/java/com/api/springapi/config/CorsConfig.java
@@ -6,24 +6,14 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods(
-                                "HEAD",
-                                "GET",
-                                "PUT",
-                                "POST",
-                                "DELETE",
-                                "PATCH",
-                                "OPTIONS"
-                        );
-            }
-        };
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("https://admin-myaichat.io")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Origin", "Content-Type", "Authorization")
+                .allowCredentials(true);
     }
 }
