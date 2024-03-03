77e6fb95448844a3a633328924e6a47ebbd45982
Francis KOUAHO <f.kouaho@skipperndt.com>
2024-03-03 13:48:41 +0100

    ok

--- dashboard/components/atoms/loadingspinner/loadingSpinner.tsx
+++ dashboard/components/atoms/loadingspinner/loadingSpinner.tsx
@@ -1,16 +1,10 @@
 import {NextPage} from 'next'
-import {LineWave} from 'react-loader-spinner'
 
 const LoadingSpinner: NextPage = () => {
     return (
-        <LineWave
-            height='200'
-            width='200'
-            wrapperClass='c-loading-spinner'
-            color="white"
-            ariaLabel="line-wave"
-            visible={true}
-        />
+        <div className="flex justify-center items-center h-full">
+            please wait...
+        </div>
     );
 };
 
